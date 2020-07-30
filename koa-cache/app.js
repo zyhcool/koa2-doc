1ff620c59719282af8eb5575cb0bfcf4c31d13bc
zyhcool <13105027620@163.com>
2020-07-30 18:05:19 +0800

    d

--- koa-cache/app.js
+++ koa-cache/app.js
@@ -1,11 +1,25 @@
 const Koa = require('koa')
-const Static = require('koa-static')
-const path = require('path')
+const path = require('path');
+const static = require('koa-static')
+const { createReadStream } = require('fs');
 
 let app = new Koa();
-app.use(Static(path.join(__dirname, './static'), {
-    maxAge: 10 * 1000, // 10s
+
+app.use(static(path.join(__dirname, './static'), {
+    maxage: 10 * 1000, // 10s
 }))
+
+// app.use(async (ctx, next) => {
+//     ctx.response.type = path.extname(ctx.path);
+//     if (ctx.path === '/') {
+//         ctx.res.setHeader('Content-Type', 'text/html')
+//         ctx.body = createReadStream(path.join(__dirname, 'static/index.html'))
+//     } else {
+//         ctx.res.setHeader('Cache-Control', 'max-age=10s')
+//         ctx.body = createReadStream(path.join(__dirname, 'static', ctx.path))
+//     }
+//     await next();
+// })
 
 const PORT = 5000
 app.listen(PORT, () => {
